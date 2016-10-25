414d2aac360a3a7a20c8393c93588795c9f69da2
Pavel Lobashov <shockwavenn@gmail.com>
2016-10-25 16:41:49 +0300

    Add LoggerHelper

--- lib/onlyoffice_testrail_wrapper.rb
+++ lib/onlyoffice_testrail_wrapper.rb
@@ -1,3 +1,4 @@
 require_relative 'testrail_api/testrail'
 require_relative 'testrail_api/helpers/hash_helper'
+require_relative 'testrail_api/helpers/logger_helper'
 require_relative 'testrail_api/helpers/string_helper'
