a5856d0a819462537065d0dd9031194fcad0af66
Pavel Lobashov <shockwavenn@gmail.com>
2021-02-05 16:08:28 +0300

    Use select instead of filter so ruby 2.5 compatible
    This alias was added in ruby 2.6

--- spec/onlyoffice_testrail_wrapper/testrail_project/runs_older_than_days_spec.rb
+++ spec/onlyoffice_testrail_wrapper/testrail_project/runs_older_than_days_spec.rb
@@ -11,13 +11,13 @@ describe OnlyofficeTestrailWrapper::TestrailProject, '#runs_older_than_days' do
 
   it 'runs_older_than_days by default returns no closed runs' do
     runs = project.runs_older_than_days(1)
-    filtered = runs.filter(&:is_completed)
+    filtered = runs.select(&:is_completed)
     expect(filtered).to be_empty
   end
 
   it 'runs_older_than_days with closed_run enable return closed runs' do
     runs = project.runs_older_than_days(1, not_closed: false)
-    filtered = runs.filter(&:is_completed)
+    filtered = runs.select(&:is_completed)
     expect(filtered).not_to be_empty
   end
 end
